a3d13dcf75d5ccf66f3cb5744e17308392650238
Maxswell Diniz <maxswellsousadiniz@yahoo.com>
2021-10-15 18:00:16 -0300

    Gerando Busca de Cep no projeto MW.Mecânica

--- OficinaMecanica/src/view/Cliente.java
+++ OficinaMecanica/src/view/Cliente.java
@@ -1,19 +1,38 @@
 package view;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.SystemColor;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Iterator;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDesktopPane;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import Atxy2k.CustomTextField.RestrictedTextField;
 import model.DAO;
+import net.proteanit.sql.DbUtils;
 
 public class Cliente extends JDialog {
 	private JTextField txtIdCli;
@@ -24,7 +43,6 @@ public class Cliente extends JDialog {
 	private JTextField txtCep;
 	private JTextField txtCpf;
 	private JTextField txtCidade;
-	private JTextField txtEstado;
 	private JTextField txtBairro;
 	private JTextField txtComplemento;
 	private JTextField txtNu;
@@ -50,6 +68,8 @@ public class Cliente extends JDialog {
 	 * Create the dialog.
 	 */
 	public Cliente() {
+		getContentPane().setBackground(SystemColor.activeCaption);
+		setBackground(Color.BLACK);
 		setTitle("MW.Mec\u00E2nica - Cliente");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Cliente.class.getResource("/img/carro1.png")));
 		setResizable(false);
@@ -58,111 +78,106 @@ public class Cliente extends JDialog {
 		getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("ID");
-		lblNewLabel.setBounds(30, 123, 46, 14);
+		lblNewLabel.setBounds(24, 158, 46, 14);
 		getContentPane().add(lblNewLabel);
 		
 		txtIdCli = new JTextField();
 		txtIdCli.setEnabled(false);
-		txtIdCli.setBounds(60, 120, 46, 20);
+		txtIdCli.setBounds(54, 155, 46, 20);
 		getContentPane().add(txtIdCli);
 		txtIdCli.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Nome");
-		lblNewLabel_1.setBounds(10, 165, 46, 14);
+		lblNewLabel_1.setBounds(10, 186, 46, 14);
 		getContentPane().add(lblNewLabel_1);
 		
 		txtCliente = new JTextField();
-		txtCliente.setBounds(60, 162, 116, 20);
+		txtCliente.setBounds(60, 183, 116, 20);
 		getContentPane().add(txtCliente);
 		txtCliente.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Telefone");
-		lblNewLabel_2.setBounds(184, 168, 62, 14);
+		lblNewLabel_2.setBounds(184, 189, 62, 14);
 		getContentPane().add(lblNewLabel_2);
 		
 		txtFone = new JTextField();
-		txtFone.setBounds(236, 165, 86, 20);
+		txtFone.setBounds(236, 186, 86, 20);
 		getContentPane().add(txtFone);
 		txtFone.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Endere\u00E7o");
-		lblNewLabel_3.setBounds(10, 196, 62, 14);
+		lblNewLabel_3.setBounds(10, 220, 62, 14);
 		getContentPane().add(lblNewLabel_3);
 		
 		txtEndereco = new JTextField();
-		txtEndereco.setBounds(70, 193, 191, 20);
+		txtEndereco.setBounds(70, 217, 191, 20);
 		getContentPane().add(txtEndereco);
 		txtEndereco.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Email");
-		lblNewLabel_4.setBounds(328, 168, 62, 14);
+		lblNewLabel_4.setBounds(328, 189, 62, 14);
 		getContentPane().add(lblNewLabel_4);
 		
 		txtEmail = new JTextField();
-		txtEmail.setBounds(378, 165, 86, 20);
+		txtEmail.setBounds(378, 186, 86, 20);
 		getContentPane().add(txtEmail);
 		txtEmail.setColumns(10);
 		
 		JLabel lblNewLabel_5 = new JLabel("CEP");
-		lblNewLabel_5.setBounds(131, 123, 46, 14);
+		lblNewLabel_5.setBounds(125, 158, 46, 14);
 		getContentPane().add(lblNewLabel_5);
 		
 		txtCep = new JTextField();
-		txtCep.setBounds(162, 120, 86, 20);
+		txtCep.setBounds(156, 155, 86, 20);
 		getContentPane().add(txtCep);
 		txtCep.setColumns(10);
 		
 		JLabel lblNewLabel_6 = new JLabel("CPF");
-		lblNewLabel_6.setBounds(475, 168, 31, 14);
+		lblNewLabel_6.setBounds(475, 189, 31, 14);
 		getContentPane().add(lblNewLabel_6);
 		
 		txtCpf = new JTextField();
-		txtCpf.setBounds(516, 165, 76, 20);
+		txtCpf.setBounds(516, 186, 76, 20);
 		getContentPane().add(txtCpf);
 		txtCpf.setColumns(10);
 		
 		JLabel lblNewLabel_7 = new JLabel("Cidade");
-		lblNewLabel_7.setBounds(10, 224, 46, 14);
+		lblNewLabel_7.setBounds(10, 248, 46, 14);
 		getContentPane().add(lblNewLabel_7);
 		
 		txtCidade = new JTextField();
-		txtCidade.setBounds(70, 221, 191, 20);
+		txtCidade.setBounds(70, 245, 191, 20);
 		getContentPane().add(txtCidade);
 		txtCidade.setColumns(10);
 		
-		JLabel lblNewLabel_8 = new JLabel("Estado");
-		lblNewLabel_8.setBounds(276, 224, 46, 14);
+		JLabel lblNewLabel_8 = new JLabel("UF");
+		lblNewLabel_8.setBounds(296, 248, 46, 14);
 		getContentPane().add(lblNewLabel_8);
 		
-		txtEstado = new JTextField();
-		txtEstado.setBounds(331, 221, 86, 20);
-		getContentPane().add(txtEstado);
-		txtEstado.setColumns(10);
-		
 		JLabel lblNewLabel_9 = new JLabel("Bairro");
-		lblNewLabel_9.setBounds(388, 196, 46, 14);
+		lblNewLabel_9.setBounds(388, 217, 46, 14);
 		getContentPane().add(lblNewLabel_9);
 		
 		txtBairro = new JTextField();
-		txtBairro.setBounds(444, 193, 142, 20);
+		txtBairro.setBounds(444, 214, 142, 20);
 		getContentPane().add(txtBairro);
 		txtBairro.setColumns(10);
 		
 		JLabel lblNewLabel_10 = new JLabel("Complemento");
-		lblNewLabel_10.setBounds(10, 255, 97, 14);
+		lblNewLabel_10.setBounds(10, 276, 97, 14);
 		getContentPane().add(lblNewLabel_10);
 		
 		txtComplemento = new JTextField();
-		txtComplemento.setBounds(99, 252, 191, 20);
+		txtComplemento.setBounds(99, 273, 191, 20);
 		getContentPane().add(txtComplemento);
 		txtComplemento.setColumns(10);
 		
 		JLabel lblNewLabel_11 = new JLabel("N\u00FAmero");
-		lblNewLabel_11.setBounds(271, 196, 46, 14);
+		lblNewLabel_11.setBounds(271, 217, 46, 14);
 		getContentPane().add(lblNewLabel_11);
 		
 		txtNu = new JTextField();
-		txtNu.setBounds(321, 193, 40, 20);
+		txtNu.setBounds(321, 214, 40, 20);
 		getContentPane().add(txtNu);
 		txtNu.setColumns(10);
 		
@@ -173,15 +188,155 @@ public class Cliente extends JDialog {
 				adicionarCliente();
 			}
 		});
-		btnAdicionarCliente.setBounds(255, 283, 89, 23);
+		btnAdicionarCliente.setBounds(253, 304, 89, 23);
 		getContentPane().add(btnAdicionarCliente);
+		
+		txtPesquisar = new JTextField();
+		txtPesquisar.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				//evento de digitar pesquisa igual do google de sugestoes de nomes de forma automatica
+				pesquisarCliente();
+			}
+		});
+		txtPesquisar.setBounds(175, 12, 197, 20);
+		getContentPane().add(txtPesquisar);
+		txtPesquisar.setColumns(10);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setIcon(new ImageIcon(Cliente.class.getResource("/img/pesquisar.png")));
+		btnNewButton.setBounds(125, 11, 32, 32);
+		getContentPane().add(btnNewButton);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBounds(10, 54, 582, 93);
+		getContentPane().add(desktopPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBackground(SystemColor.activeCaptionBorder);
+		scrollPane.setBounds(0, 0, 582, 93);
+		desktopPane.add(scrollPane);
+		
+		tableCliente = new JTable();
+		scrollPane.setViewportView(tableCliente);
+		
+		cboUf = new JComboBox();
+		cboUf.setModel(new DefaultComboBoxModel(new String[] {"", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"}));
+		cboUf.setBounds(321, 244, 54, 22);
+		getContentPane().add(cboUf);
+		
+		// uso da biblioteca Atxy2k para validaçaõ do campo cep
+		RestrictedTextField validar = new RestrictedTextField(txtCep);
+		
+		JButton btnCep = new JButton("Buscar");
+		btnCep.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Buscar por cep 
+				if (txtCep.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Informe o CEP");
+					txtCep.requestFocus();
+					// se a condição for verdadeira o else sera ignorado
+			}else {
+				// Buscar CEP
+				buscarCep();
+				
+			}
+			}
+		});
+		btnCep.setBounds(321, 154, 89, 23);
+		getContentPane().add(btnCep);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// limpando busca por cep
+				limpar();
+			}
+		});
+		btnLimpar.setBounds(424, 154, 89, 23);
+		getContentPane().add(btnLimpar);
+		validar.setOnlyNums(true);
+		validar.setLimit(8); 
 
-	}
+	}// Criando ligação de busca dom4j
+	private void buscarCep() {
+		String Logradouro="";
+		String tipoLogradouro="";
+		String resultado=null;
+		String cep=txtCep.getText();
+		try {
+			URL url = new URL("http://cep.republicavirtual.com.br/web_cep.php?cep=" + cep + "&formato=xml");
+		      SAXReader xml = new SAXReader();
+		      Document documento = xml.read(url);
+		      Element root = documento.getRootElement();
+		      for (Iterator<Element> it = root.elementIterator(); it.hasNext();) {
+		          Element element = it.next();
+		          // Se ele encontrar o nome da variavel farar o preenchimento de busca dos campos
+		          if (element.getQualifiedName().equals("cidade")) {
+		        	  //identificando e extraindo um documento do xml
+		        	  txtCidade.setText(element.getText());	  
+		          }
+		          if (element.getQualifiedName().equals("bairro")) {
+		        	  //identificando e extraindo um documento do xml
+		        	  txtBairro.setText(element.getText());	  
+		          }
+		          // Identificando Uf
+		          if (element.getQualifiedName().equals("uf")) {
+		        	  //identificando e extraindo um documento do xml
+		        	  cboUf.setSelectedItem(element.getText());
+		          }
+		         
+		          if (element.getQualifiedName().equals("tipo_logradouro")) {
+		        	  //identificando e extraindo um documento do xml
+		        	  tipoLogradouro = element.getText();
+		        	  
+		          }
+		          if (element.getQualifiedName().equals("logradouro")) {
+		        	  //identificando e extraindo um documento do xml
+		        	  Logradouro = element.getText();
+		        	  
+		          }
+		          //setar o campo endereco
+		          txtEndereco.setText(tipoLogradouro + " " + Logradouro);
+		        	// Alertando se o cep não existir
+		          if (element.getQualifiedName().equals("resultado")) {
+		          resultado = element.getText();
+		          if (resultado.equals("1")) {
+		        	  // Colocando icone de ok de cep encontado
+		        	  /*lblStatus.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/okcep.png")));*/
+		          }else
+		        	  JOptionPane.showMessageDialog(null, "CEP não encontrado");
+			          
+			          } 
+		          
+		          }  
+			        	 
+		        	 
+		        	  
+		          
+		      
+		
+		
+		} catch (Exception e) {
+			System.out.println(e);
+			}
+			}
+
+
+
+	
+
+		
+	
+
 	// fim do construtor importe a classe DAO crtl + shift + o
 
 
 		// criar objeto para reutilizar a classe DAO(Conexao com o banco)
 		DAO dao = new DAO();
+		private JTextField txtPesquisar;
+		private JTable tableCliente;
+		private JComboBox cboUf;
 		//metodo para inserir um novo cliente
 		private void adicionarCliente() {
 			//validacao dos campos obrigatorios
@@ -222,10 +377,7 @@ public class Cliente extends JDialog {
 						JOptionPane.showMessageDialog(null, "Preencha a Cidade do cliente ");
 						txtCidade.requestFocus();
 						}
-				 else if (txtEstado.getText().isEmpty()) {
-						JOptionPane.showMessageDialog(null, "Preencha o Estado ");
-						txtEstado.requestFocus();
-						}
+				
 			
 		//////////////// Lógica Principal ////////////////////////////////////////////////////////	
 				 else {
@@ -249,8 +401,7 @@ public class Cliente extends JDialog {
 							pst.setString(8, txtCidade.getText());
 							pst.setString(9, txtBairro.getText());
 							pst.setString(10, txtComplemento.getText());
-							pst.setString(11, txtEstado.getText());
-							
+						
 							//executar a query confirmando a inclusao do cliente
 							int confirma = pst.executeUpdate();
 							if (confirma == 1) {
@@ -270,6 +421,35 @@ public class Cliente extends JDialog {
 							}
 							}
 		}
+		// Pesquisa avançada de clientes usando abiblioteca rs2xml
+		
+		private void pesquisarCliente() {
+
+			String read = "select * from clientes where nome like ? order by nome";
+			try {
+			//abrir a conexao com o banco
+			Connection con = dao.conectar();
+			//preparar a query(instrucao sql) para pesquisar no banco
+			PreparedStatement pst = con.prepareStatement(read);
+			//substituir o parametro(?) Atencao ao % para completar a query
+			pst.setString(1, txtPesquisar.getText() + "%");
+			//obter os dados do banco (resultado)
+			ResultSet rs = pst.executeQuery();
+			//popular(preencher) a tabela com os dados do banco
+			tableCliente.setModel(DbUtils.resultSetToTableModel(rs));
+			
+		} catch (Exception e) {
+			System.out.println(e);
+			
+		}
+		}
+		
+		
+		
+		
+		
+		
+		
 		//limpar os campos e gerenciar os botoes
 		private void limpar() {
 		txtCliente.setText(null);
@@ -282,7 +462,8 @@ public class Cliente extends JDialog {
 		txtCidade.setText(null);
 		txtBairro.setText(null);
 		txtComplemento.setText(null);
-		txtEstado.setText(null);
+		cboUf.setSelectedItem(null);
+	
 }
 }
 
